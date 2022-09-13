32b1d8e36495e41f69cb8bdc5fa436f9acaef0d1
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-09-13 14:45:54 -0400

    core-abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApplication.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import com.cydeo.service.CommentService;
+
+public class CommentApplication {
+    public static void main(String[] args) {
+
+        // CommentService commentService
+
+
+    }
+}
