6a6946bef92b3646216ac6b83dd18b5317c0f45e
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-09-19 19:10:16 -0400

    MVC Model-View-Controller. Model. Thymeleaf template engine

---
+++ spring-09-mvc-model/src/main/java/com/cydeo/MyModelApp.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyModelApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyModelApp.class, args);
+	}
+
+}
