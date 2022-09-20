8f6c8ff742c30714c40ee91627738f34462b577d
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-09-20 11:54:35 -0400

    MVC Model-View-Controller. Model. Thymeleaf template engine

--- spring-09-mvc-model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-mvc-model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,15 +1,42 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homepage() {
+    public String homepage(Model model) {
         String name ="ABCD efg";
 
-        return thymeleaf;
+        model.addAttribute("name", "Cydeo");
+        model.addAttribute("course", "MVC");
+
+        String subject ="Spring boot";
+        model.addAttribute("subject", subject);
+
+        int studentID = new Random().nextInt();
+        model.addAttribute("studentID", studentID);
+
+        List<Integer> numbers =new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        model.addAttribute("numbers", numbers);
+
+        LocalDate dt =LocalDate.now();
+        model.addAttribute("date", dt);
+
+        //method from Model Interface
+        return"/student/welcome";
     }
 }
