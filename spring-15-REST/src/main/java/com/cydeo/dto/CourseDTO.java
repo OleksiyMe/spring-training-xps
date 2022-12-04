fa602a9b8b3e611ffb2c07d4579f4f056a078a7e
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-12-03 23:28:39 -0500

    adding xml responce to createUser() method

--- spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
+++ spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -2,12 +2,14 @@ package com.cydeo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CourseDTO {
 
 
